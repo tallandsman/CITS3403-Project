9536398c6d77c2c7bcbd2a2de429a3b2e4999d18
Tal Landsman <tallandsman00@gmail.com>
2022-05-18 12:57:32 +0800

    Basic db implemented

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 # table to store new user details
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -12,6 +14,14 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    # generates a password hash - the original password isn't stored 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # verifies password
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 # table to store game stats
 class Game_Statistics(db.Model):
@@ -31,3 +41,8 @@ class Puzzle(db.Model):
 
     def __repr__(self):
         return # to be completed #
+
+# to delete after testing (?)
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
