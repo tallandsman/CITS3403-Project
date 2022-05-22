0d44e2cea977edf3820e1b367d15048c28004b82
Alison0816 <22835304@student.uwa.edu.au>
2022-05-22 12:39:04 +0800

    stat.js

--- app/static/scripts/statistics.js
+++ app/static/scripts/statistics.js
@@ -14,3 +14,47 @@ function openStat(event, tabName) {
     event.currentTarget.className += " active";
 }
 
+// Sorting table by date
+// TODO: Not working :(
+// document.getElementById("sort-date").addEventListener("click", sortByDate);
+
+// function convertDate(d) {
+//     var p = d.split("/");
+//     return +(p[2]+p[1]+p[0]);
+// }
+
+// function sortByDate() {
+//     var tbody = document.querySelector("#results tbody");
+//     var rows = [].slice.call(tbody.querySelectorAll("tr"));
+
+//     rows.sort(function(a,b) {
+//         return convertDate(a.cells[0].innerHTML) - convertDate(b.cells[0].innerHTML);
+//     });
+
+//     rows.forEach(function(v) {
+//         tbody.appendChild(v);
+//     });
+// }
+
+
+// function convertDate(d) {
+//     var p = d.split("/");
+//     return +(p[2]+p[1]+p[0]);
+//   }
+  
+//   function sortByDate() {
+//     var tbody = document.querySelector("#results tbody");
+//     // get trs as array for ease of use
+//     var rows = [].slice.call(tbody.querySelectorAll("tr"));
+    
+//     rows.sort(function(a,b) {
+//       return convertDate(a.cells[0].innerHTML) - convertDate(b.cells[0].innerHTML);
+//     });
+    
+//     rows.forEach(function(v) {
+//       tbody.appendChild(v); // note that .appendChild() *moves* elements
+//     });
+//   }
+  
+//   document.querySelector("button").addEventListener("click", sortByDate);
+
